1b19c4db16dd075a7c176a6393d120b1f3c2dc97
Sebastien Ros <sebastienros@gmail.com>
2017-10-12 18:21:01 -0700

    Varying tasks

--- BenchmarkDb/Program.cs
+++ BenchmarkDb/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Diagnostics;
@@ -14,9 +15,8 @@ namespace BenchmarkDb
 {
     class Program
     {
-        static int Threads = 1024;
-        static int MaxTransactions = 100000000;
-        static int Counter = 0;
+        //static int Concurrency = 32;
+        static volatile int Counter = 0;
 
         const string PostgreSql = nameof(PostgreSql);
         const string MySql = nameof(MySql);
@@ -51,7 +51,7 @@ namespace BenchmarkDb
                     break;
 
                 default:
-                    Console.WriteLine($"Acceped database values: {SqlServer}, {MySql}, {PostgreSql}");
+                    Console.WriteLine($"Accepted database values: {SqlServer}, {MySql}, {PostgreSql}");
                     Environment.Exit(2);
                     break;
             }
@@ -59,37 +59,63 @@ namespace BenchmarkDb
             Console.WriteLine($"Running with {args[0]} on {connectionString}");
 
             var stopwatch = new Stopwatch();
-            stopwatch.Start();
+            var tasks = new List<Task>();
+            var stopping = false;
+            var startTime = DateTime.UtcNow;
+            var lastDisplay = DateTime.UtcNow;
+            var lastNewTask = DateTime.UtcNow;
 
-            var tasks = Enumerable.Range(1, Threads).Select(async i =>
+            while (!stopping)
             {
-                while (Interlocked.Add(ref Counter, 1) < MaxTransactions)
-                {
-                    using (var connection = factory.CreateConnection())
-                    {
-                        connection.ConnectionString = connectionString;
-                        var results = await connection.QueryAsync("SELECT id,message FROM fortune");
+                Thread.Sleep(200);
+                var now = DateTime.UtcNow;
 
-                        if (results.Count() != 12)
+                if ((now - lastDisplay) > TimeSpan.FromMilliseconds(200))
+                {
+                    Console.Write($"{tasks.Count} Threads, {Counter / (now - lastDisplay).TotalSeconds} tps                                   ");
+                    Console.CursorLeft = 0;
+                    lastDisplay = now;
+                    Counter = 0;
+
+                }
+
+                if ((now - lastNewTask) > TimeSpan.FromMilliseconds(2000))
+                {
+                    tasks.Add(Task.Run(Thing));
+
+                    async Task Thing()
+                    {
+                        while (!stopping)
                         {
-                            throw new ApplicationException();
+                            Interlocked.Add(ref Counter, 1);
+
+                            try
+                            {
+                                using (var connection = factory.CreateConnection())
+                                {
+                                    connection.ConnectionString = connectionString;
+                                    var results = await connection.QueryAsync("SELECT id,message FROM fortune");
+
+                                    if (results.Count() != 12)
+                                    {
+                                        throw new ApplicationException("Not 12");
+                                    }
+                                }
+                            }
+                            catch(Exception e)
+                            {
+                                Console.WriteLine(e);
+                            }
                         }
                     }
+
+                    lastNewTask = now;
                 }
-            }).ToList();
-
-
-            tasks.Add(Task.Delay(TimeSpan.FromSeconds(10)));
-
-            Task.WhenAny(tasks).GetAwaiter().GetResult();
-
-            if (Counter <= 1)
-            {
-                throw new ApplicationException("Connection strings seems wrong");
             }
 
-            stopwatch.Stop();
-            Console.WriteLine($"{Counter} transactions in {stopwatch.Elapsed.TotalSeconds} seconds, {Counter / stopwatch.Elapsed.TotalSeconds} tps");
+            Task.WhenAll(tasks).GetAwaiter().GetResult();
+            
+            
         }
     }
 }
---
+++ BenchmarkDbFull/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+using Npgsql;
+
+namespace BenchmarkDbFull
+{
+    class Program
+    {
+        static int Threads = 1024;
+        static int Concurrency = 32;
+        static int MaxTransactions = 100000000;
+        static int Counter = 0;
+
+        const string PostgreSql = nameof(PostgreSql);
+        const string MySql = nameof(MySql);
+        const string SqlServer = nameof(SqlServer);
+
+        static object synlock = new object();
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("usage: database connectionstring");
+                Environment.Exit(1);
+            }
+
+            var connectionString = args[1];
+
+            DbProviderFactory factory = null;
+
+            switch (args[0])
+            {
+                case PostgreSql:
+                    factory = NpgsqlFactory.Instance;
+                    break;
+
+                case MySql:
+                    factory = MySqlClientFactory.Instance;
+                    break;
+
+                case SqlServer:
+                    factory = SqlClientFactory.Instance;
+                    break;
+
+                default:
+                    Console.WriteLine($"Acceped database values: {SqlServer}, {MySql}, {PostgreSql}");
+                    Environment.Exit(2);
+                    break;
+            }
+
+            Console.WriteLine($"Running with {args[0]} on {connectionString}");
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var tasks = Enumerable.Range(1, Concurrency).Select(i => Task.Run(async () =>
+            {
+                while (Interlocked.Add(ref Counter, 1) < MaxTransactions)
+                {
+                    using (var connection = factory.CreateConnection())
+                    {
+                        connection.ConnectionString = connectionString;
+                        var results = await connection.QueryAsync("SELECT id,message FROM fortune");
+
+                        if (results.Count() != 12)
+                        {
+                            throw new ApplicationException();
+                        }
+                    }
+                }
+            })).ToList();
+
+            tasks.Add(Task.Delay(TimeSpan.FromSeconds(10)));
+
+            Task.WhenAny(tasks).GetAwaiter().GetResult();
+
+            if (Counter <= 1)
+            {
+                throw new ApplicationException("Connection strings seems wrong");
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"{Counter} transactions in {stopwatch.Elapsed.TotalSeconds} seconds, {Counter / stopwatch.Elapsed.TotalSeconds} tps");
+        }
+    }
+}
